4c4e5700c046b922c696cb7923b2f4150155044b
System Administrator <timofeibalaban@gmail.com>
2021-02-28 13:17:52 -0500

    add json

--- src/test/java/DashboardRunner.java
+++ src/test/java/DashboardRunner.java
@@ -1,15 +1,16 @@
 import io.cucumber.junit.Cucumber;
-        import io.cucumber.junit.CucumberOptions;
-        import org.junit.runner.RunWith;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty" , "html:target/DashboardRunner",
-                "rerun:target/Rerun_Failed/DashboardRunner.txt"},
+        plugin = {"pretty", "html:target/DashboardRunner",
+                "rerun:target/Rerun_Failed/DashboardRunner.txt",
+                "json:target/cucumber.json"},
         features = "src/main/resources/features/manageAccess.feature",
         glue = {"steps"}
         , stepNotifications = true
-       // tags="@regression"
+        // tags="@regression"
         //   tags="@regression and @shakeout"
 )
 
